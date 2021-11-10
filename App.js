a9bae8c65f9fdface60a87edbd53417e558c804b
서영우 <seoyeong-u@seoyeong-us-Mac-mini.local>
2021-11-10 20:08:04 +0900

    commit

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+/** Map publish */
+import PubMapMain from './component/screen/map/PubMapMain';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    //StackNavigator
+    const Stack = createStackNavigator();
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator 
+                initialRouteName={"PubMapMain"}
+                screenOptions={{
+                    headerShown: false,
+                    headerStyle:{
+                        backgroundColor : "pink",
+                        borderBottomColor : "pink",
+                        shadowColor:"pink"
+                    },
+                    headerTintColor:"white",
+                    headerBackTitleVisible:false
+                }
+            }>
+                <Stack.Screen name="PubMapMain" component={PubMapMain}/>
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ component/screen/map/PubMapMain.js
@@ -0,0 +1,167 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+/** 상단 스테이터스 바 */
+import { StatusBar } from 'expo-status-bar';
+
+/** React-Native-Map lib */
+import MapView, { Marker, PROVIDER_GOOGLE, animateToRegion, Callout} from 'react-native-maps';
+
+
+
+export default function PubMapMain({navigation, route, options, back}){
+    
+
+    return (
+        <SafeAreaView style={styles.container}>
+            {/** 상단 스테이터스 바 */}
+            <StatusBar barStyle="dark-content" backgroundColor={'transparent'} translucent={true} />
+
+            {/** Google Map View */}
+            <MapView
+                style={styles.map}
+                customMapStyle={mapStyle}
+                provider={PROVIDER_GOOGLE}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.02,
+                    longitudeDelta: 0.02,
+                }}
+            >
+
+            </MapView>
+        </SafeAreaView>
+    )
+};
+
+const styles = StyleSheet.create({
+    container : {
+        ...StyleSheet.absoluteFill,
+    }, 
+    map : {
+        ...StyleSheet.absoluteFill,
+    }
+
+})
+
+const mapStyle = [
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "hue": "#0066ff"
+            },
+            {
+                "saturation": 74
+            },
+            {
+                "lightness": 100
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "weight": 0.6
+            },
+            {
+                "saturation": -85
+            },
+            {
+                "lightness": 61
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "color": "#5f94ff"
+            },
+            {
+                "lightness": 26
+            },
+            {
+                "gamma": 5.86
+            }
+        ]
+    }
+];
+
